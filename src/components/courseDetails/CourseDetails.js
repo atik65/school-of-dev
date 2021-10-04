5cf62103d8b7b5f6843e0d749c0d659246453bdc
Atik Hasan <atikhasan801@gmail.com>
2021-10-04 20:12:13 +0600

    routing prb fixed

--- src/components/courseDetails/CourseDetails.js
+++ src/components/courseDetails/CourseDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Redirect, useParams } from "react-router";
 import image from "../../images/undraw_programming_2svr.svg";
+import PageNotFound from "../error/PageNotFound";
 import "./courseDetails.css";
 
 const CourseDetails = () => {
@@ -17,26 +18,30 @@ const CourseDetails = () => {
   useEffect(() => {
     const filterd = courses?.find((item) => item.id === parseInt(userid));
     setFiltered(filterd);
+    console.log(filterd, userid);
   }, [userid, courses]);
-  //   const { course, image, price, id } = filterd;
 
-  return (
-    <div>
-      <div className="row courseDetails">
-        <div className="col-md-6 pt-5">
-          <img className="courseDetials-img-svg" src={image} alt="" />
-        </div>
-        <div className="col-md-6">
-          <h2 className="pb-3">Course: {filterd?.course}</h2>
-          <img className="courseDetials-img" src={filterd?.image} alt="" />
-          <h3 className="py-2">Price: {filterd?.price} Taka</h3>
-          <button className="btn mb-2 btn-sm btn-outline-primary">
-            Purchase Now
-          </button>
+  if (filterd) {
+    return (
+      <div>
+        <div className="row courseDetails">
+          <div className="col-md-6 pt-5">
+            <img className="courseDetials-img-svg" src={image} alt="" />
+          </div>
+          <div className="col-md-6">
+            <h2 className="pb-3">Course: {filterd?.course}</h2>
+            <img className="courseDetials-img" src={filterd?.image} alt="" />
+            <h3 className="py-2">Price: {filterd?.price} Taka</h3>
+            <button className="btn mb-2 btn-sm btn-outline-primary">
+              Purchase Now
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <PageNotFound />;
+  }
 };
 
 export default CourseDetails;
